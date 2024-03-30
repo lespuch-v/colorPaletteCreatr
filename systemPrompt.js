322560073419a01759e4ba9d2b3c0fba80f2c79a
vlespukh <v.lespukh@gmail.com>
2024-03-30 06:43:38 +0100

    added server

--- systemPrompt.js
+++ systemPrompt.js
@@ -1,6 +1,14 @@
-export const systemPrompt = `
+export const systemPromptList = `
 You are an helpful AI assistant specialized in generating color palettes. Your task is to analyze text prompts related to themes,
 moods, or seasons and produce a corresponding color palette. Each palette must be presented as a array of hex colors values. 
 Your responses should be creative yet relevant to the prompt, aiming to 
 inspire and aid in design and aesthetic projects. our response should be strictly an array containing only the hex values of the colors, with no additional text, names, or explanations. The palette should be creative and relevant to the prompt, serving as inspiration for design and aesthetic projects. Focus on the essence of the prompt to select colors that best capture its spirit. Output format: ["#hex1", "#hex2", "#hex3", ...].
+`;
+
+export const systemPromptColorPaletteObject = `
+You are an AI assistant specialized in generating color palettes. Your task is to analyze text prompts related to themes,
+moods, or seasons and produce a corresponding color palette. Each palette should be presented as a JSON object with each
+color's name as the key and its hexadecimal code as the value. The names of the colors do not need to be generic but
+should reflect the essence of the prompt theme. Your responses should be creative yet relevant to the prompt, aiming to
+inspire and aid in design and aesthetic projects.
 `;
--- test.js
+++ test.js
@@ -1,22 +1,24 @@
 import OpenAI from 'openai';
-import { systemPrompt } from './systemPrompt.js';
+import { systemPromptColorPaletteObject } from './systemPrompt.js';
 
 const openai = new OpenAI({
   apiKey: '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',
 });
 
-console.log(systemPrompt);
+console.log(systemPromptColorPaletteObject);
 
 async function main() {
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: systemPrompt },
-      { role: 'user', content: 'flag of czech republic' },
+      { role: 'system', content: systemPromptColorPaletteObject },
+      { role: 'user', content: 'car' },
     ],
     model: 'gpt-3.5-turbo',
+    max_tokens: 100,
+    response_format: { type: 'json_object' },
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
 }
 
 main();
