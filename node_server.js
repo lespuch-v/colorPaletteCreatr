a4bd464ed4fc3e1ce352ab9cab49275f4d6009e2
vlespukh <v.lespukh@gmail.com>
2024-04-01 08:23:19 +0200

    - refactoring

--- node_server.js
+++ node_server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
-import { systemPromptColorPaletteObject } from './systemPrompt.js';
+import {systemPromptColorPaletteObject} from './systemPrompt.js';
 
 const app = express();
 dotenv.config();
@@ -15,37 +15,37 @@ app.use(cors());
 app.use(express.json());
 const port = 3000;
 const openai = new OpenAI({
-  apiKey: process.env.API_KEY,
+    apiKey: process.env.API_KEY,
 });
 
 app.post('/color-palette', async (req, res) => {
-  try {
-    const userInput = req.body.user_input;
-    console.log('User input:', userInput);
+    try {
+        const userInput = req.body.user_input;
+        console.log('User input:', userInput);
 
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: 'system', content: systemPromptColorPaletteObject },
-        { role: 'user', content: userInput },
-      ],
-      model: 'gpt-3.5-turbo',
-      max_tokens: 100,
-      response_format: { type: 'json_object' },
-    });
+        const completion = await openai.chat.completions.create({
+            messages: [
+                {role: 'system', content: systemPromptColorPaletteObject},
+                {role: 'user', content: userInput},
+            ],
+            model: 'gpt-3.5-turbo',
+            max_tokens: 100,
+            response_format: {type: 'json_object'},
+        });
 
-    const colorPalette = completion.choices[0].message.content;
-    res.json(colorPalette); // Send the JSON object as the response
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Failed to fetch color palette' });
-  }
+        const colorPalette = completion.choices[0].message.content;
+        res.json(colorPalette); // Send the JSON object as the response
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Failed to fetch color palette'});
+    }
 });
 
 app.get('/test', (req, res) => {
-  res.json({ message: 'Hello from test GET route!' });
+    res.json({message: 'Hello from test GET route!'});
 });
 
 app.listen(port, () => {
-  console.log('Server started!')
-  console.log(`Server listening on port ${port}`);
+    console.log('Server started!');
+    console.log(`Server listening on port ${port}`);
 });
